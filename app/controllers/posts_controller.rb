23033e89de12c46b626a18f9243f50108efffc9b
Ryosuke Uchida <uchiryo7@gmail.com>
2018-02-10 16:47:13 +0900

    post controllerを編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,45 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.all
   end
 
   def show
+    @post = Post.find_by(id: params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path(@post)
+    else
+      render :new
+    end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    if @post = Post.update(post_params)
+      redirect_to posts_path(@post)
+    else
+      render edit_post_path(@post)
+    end
   end
 
   def destroy
+    @post = Post.destroy
+    redirect_to posts_path
   end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:content, :title, :youtube_url)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'posts/index'
-
-  get 'posts/show'
-
-  get 'posts/new'
-
-  get 'posts/create'
-
-  get 'posts/edit'
-
-  get 'posts/update'
-
-  get 'posts/destroy'
-
   resources :home
-  resources :maps
+  resources :posts
 
   root 'home#index'
 
